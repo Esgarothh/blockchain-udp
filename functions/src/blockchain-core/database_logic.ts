39b59e116eabf80e78cc3fe8adcc3bcbdbcb47b0
Matias Gastellu Marquez <matias.gastellu@mail.udp.cl>
2023-10-10 01:10:40 -0300

    feat(): add sendChainToServer Method
    added also the function to get the genesis block and the blockchain.

--- functions/src/blockchain-core/database_logic.ts
+++ functions/src/blockchain-core/database_logic.ts
@@ -1,10 +1,34 @@
 require("esm-hook");
-
 const fetch = require("node-fetch").default;
 import { Transaction } from "./transaction";
 import { Block } from './block'; // Import the Block type
 
-// Función para enviar un bloque al servidor
+
+
+export async function sendChainToServer(chain:Block[]) {
+  try {
+    const dataToSend = {
+      key: 'chain',
+      data: chain,
+    };
+
+    const response = await fetch('http://localhost:3000/storeData', {
+      method: 'POST',
+      body: JSON.stringify(dataToSend),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.status === 200) {
+      console.log('Chain sent to server:', chain);
+      return response.json();
+    } else {
+      console.error('Error sending chain to server:', response.statusText);
+    }
+  } catch (error:any) {
+    console.error('Error sending chain to server:', error.message);
+  }
+}
+
 export async function sendBlockToServer(block:Block) {
   try {
     const dataToSend = {
@@ -28,7 +52,6 @@ export async function sendBlockToServer(block:Block) {
   }
 }
 
-// Función para enviar una transacción al servidor
 export async function sendTransactionToServer(transaction:Transaction) {
   try {
     const dataToSend = {
@@ -49,5 +72,41 @@ export async function sendTransactionToServer(transaction:Transaction) {
     }
   } catch (error:any) {
     console.error('Error sending transaction to server:', error.message);
+  }
+}
+
+export const getGenesisBlock =  async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/genesisBlock`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      console.error('Error on genesis block request:', response.statusText);
+      return null;
+    }
+  } catch (error:any) {
+    console.error('Error on genesis block request', error.message);
+  }
+}
+export const getBlockchain =  async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/getBlockchain`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.status === 200) {
+      console.log('Blockchain received:');
+      return response.json();
+    } else {
+      console.error('Error on genesis block request:', response.statusText);
+      return null;
+    }
+  } catch (error:any) {
+    console.error('Error on genesis block request', error.message);
   }
 }
